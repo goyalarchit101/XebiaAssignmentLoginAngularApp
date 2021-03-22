f3f3a500b9bf341c730b35180b6263f03b8bb0dc
goyalarchit101 <ranadheer@example.com>
2021-03-22 15:29:40 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // database: 'mongodb://archit:archit@cluster0.i2ll3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',   //prod
+  database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
+
